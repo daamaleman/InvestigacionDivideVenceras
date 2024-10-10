8990be7feeea7658ad9cd927c2c5dbd6c3b62f59
elias <eamarin@uamv.edu.ni>
2024-10-09 21:31:20 -0600

    Agrego ejemplo 3

---
+++ Codigo/Ejemplo3/ejemplo3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Power(2, 10);
+            Console.WriteLine(result);
+
+            // Evitar que la consola se cierre inmediatamente
+            Console.WriteLine("Presiona Enter para salir...");
+            Console.ReadLine();
+        }
+
+        public static long Power(long x, int n)
+        {
+            if (n == 0)
+            {
+                return 1; // Caso base
+            }
+            long half = Power(x, n / 2);
+
+            if (n % 2 == 0)
+            {
+                return half * half; // Caso par
+            }
+            else
+            {
+                return x * half * half; // Caso impar
+            }
+        }
+
+    }
+}
